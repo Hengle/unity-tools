06d55e593302b7f22c6ce91c0b820972d955abb7
Anton Pantev <pantevanton@gmail.com>
2016-05-30 14:53:43 -0400

    Moved all the GenerateTexture code into one folder.

---
+++ Assets/GenerateTexture/Editor/GenerateTextureEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GenerateTexture))]
+public class GenerateTextureEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GenerateTexture gt = (GenerateTexture) target;
+
+        if (GUILayout.Button("Random Texture", GUILayout.ExpandWidth(false)))
+        {
+            gt.randomTexture();
+            AssetDatabase.Refresh();
+        }
+    }
+
+}
--- Assets/GenerateTexture/GenerateTexture.cs
+++ Assets/GenerateTexture/GenerateTexture.cs
@@ -1,23 +1,22 @@
 ﻿using UnityEngine;
-using UnityEditor;
 using System.IO;
 
-public class CreateRandomTexture {
+public class GenerateTexture : MonoBehaviour {
 
-    public static int width = 128;
-    public static int height = 128;
-    public static float perlinScale = 20;
-    
-    [MenuItem("Tools/Create Random Texture")]
-    private static void randomTexture()
+    public int width = 128;
+    public int height = 128;
+    public float perlinScale = 20;
+    public string fileName = "random.png";
+
+    public void randomTexture()
     {
         Texture2D texture = new Texture2D(width, height);
         PerlinHelper reds = new PerlinHelper(width, height, perlinScale);
         PerlinHelper greens = new PerlinHelper(width, height, perlinScale);
 
-        for(int x = 0; x < width; x++)
+        for (int x = 0; x < width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
                 texture.SetPixel(x, y, new Color(reds[x, y], greens[x, y], 0, 1));
             }
@@ -26,10 +25,10 @@ public class CreateRandomTexture {
         saveTexture(texture);
     }
 
-    private static void saveTexture(Texture2D texture)
+    private void saveTexture(Texture2D texture)
     {
         byte[] data = texture.EncodeToPNG();
-        BinaryWriter writer = new BinaryWriter(File.Open(Application.dataPath + "/foo.png", FileMode.Create));
+        BinaryWriter writer = new BinaryWriter(File.Open(Application.dataPath + "/" + fileName, FileMode.Create));
         writer.Write(data);
         writer.Close();
     }
