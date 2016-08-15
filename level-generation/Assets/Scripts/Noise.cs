a0abaa9dfa832685df4529a686d1a6ae8ed973fd
Anton Pantev <pantevanton@gmail.com>
2016-08-15 09:39:02 -0400

    Displaying random heights.

--- level-generation/Assets/Scripts/Noise.cs
+++ level-generation/Assets/Scripts/Noise.cs
@@ -1,29 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] foo()
+    public static float[,] generateMap(int width, int height)
     {
-        return new float[10, 10];
+        return generateMap(width, height, Environment.TickCount);
     }
 
-    public static float[,] foo(out int seed)
+    public static float[,] generateMap(int width, int height, out int seed)
     {
-        seed = 0;
-        return new float[10, 10];
+        seed = Environment.TickCount;
+        return generateMap(width, height, seed);
     }
 
     public static float[,] generateMap(int width, int height, int seed)
     {
-        float[,] map = new float[width, height];
+        Debug.Log(seed);
 
+        float[,] map = new float[width, height];
+        
         System.Random rand = new System.Random(seed);
 
         for (int i = 0; i < width; i++)
         {
             for(int j = 0; j < height; j++)
             {
-
+                map[i, j] = (float) rand.NextDouble();
             }
         }
 
---
+++ level-generation/Assets/Scripts/Terrain2D.cs
@@ -0,0 +1,40 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class Terrain2D : MonoBehaviour
+{
+    private Renderer r;
+
+    // Use this for initialization
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+
+        int width = 100;
+        int height = 100;
+
+        float[,] heights = Noise.generateMap(width, height);
+
+        Color[] colors = new Color[width * height];
+        for(int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                colors[(j * width) + i] = Color.Lerp(Color.black, Color.white, heights[i, j]);
+            }
+        }
+
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(colors);
+        texture.Apply();
+
+        r.sharedMaterial.mainTexture = texture;
+        transform.localScale = new Vector3(((float)width / height), 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
