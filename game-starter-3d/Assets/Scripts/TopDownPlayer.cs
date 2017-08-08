c20f342f378f5d6a8b9a71df258184f32461b9d3
Anton Pantev <pantevanton@gmail.com>
2017-08-08 17:33:23 -0400

    Added 3d game starter project.

---
+++ game-starter-3d/Assets/Scripts/TopDownPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayer : MonoBehaviour {
+    public float speed = 21f;
+    public float turnSmooth = 0.3f;
+    public float maxTurnSpeed = 540f;
+
+    public Transform model;
+
+    private float horAxis = 0f;
+    private float vertAxis = 0f;
+    private float turnVelY = 0.0F;
+
+    private MovementAIRigidbody rb;
+
+	void Start () {
+        rb = GetComponent<MovementAIRigidbody>();
+	}
+	
+	void Update () {
+        horAxis = Input.GetAxisRaw("Horizontal");
+        vertAxis = Input.GetAxisRaw("Vertical");
+        rotateChar();
+    }
+
+    private void rotateChar()
+    {
+        if (horAxis != 0 || vertAxis != 0)
+        {
+            float targetYAngle = Mathf.Atan2(-vertAxis, horAxis) * Mathf.Rad2Deg;
+            Vector3 eulerAngles = model.localEulerAngles;
+            eulerAngles.y = Mathf.SmoothDampAngle(eulerAngles.y, targetYAngle, ref turnVelY, turnSmooth, maxTurnSpeed);
+            model.localEulerAngles = eulerAngles;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rotateChar();
+        moveChar();
+    }
+
+    private void moveChar()
+    {
+        Vector3 vel = (transform.right * horAxis) + (transform.forward * vertAxis);
+        vel = vel.normalized * speed;
+        rb.velocity = vel;
+    }
+}
