4f6a5b3eb3162b6e42a3bda11171693d881bdeb5
Anton Pantev <pantevanton@gmail.com>
2018-03-15 12:55:06 -0400

    Adding a spawner.

---
+++ game-starter/Assets/Scripts/Standard/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Transform prefab;
+    public float spawnRate = 2f;
+    public float spawnChance = 0.5f;
+
+    private float nextSpawn;
+
+    void Start()
+    {
+        nextSpawn = Time.time + spawnRate;
+    }
+
+    void Update()
+    {
+        if (nextSpawn < Time.time)
+        {
+            if (Random.value < spawnChance)
+            {
+                Spawn();
+            }
+            nextSpawn = Time.time + spawnRate;
+        }
+    }
+
+    public void Spawn()
+    {
+        Instantiate(prefab, prefab.position + transform.position, Quaternion.identity);
+    }
+}
