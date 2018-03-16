a0f0b1982b14af12d90aea49aa4d078319848885
Anton Pantev <pantevanton@gmail.com>
2018-03-16 00:05:53 -0400

    Adding start screen example.

---
+++ game-starter/Assets/Scripts/Standard/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+	public string playScene;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(playScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
