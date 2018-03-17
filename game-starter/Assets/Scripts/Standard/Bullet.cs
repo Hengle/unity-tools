87b7f913d24fb6a8e4728fcee5dafc372a75ae0f
Anton Pantev <pantevanton@gmail.com>
2018-03-17 01:40:38 -0400

    Adding a bullet example.

---
+++ game-starter/Assets/Scripts/Standard/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector3 velocity;
+    public float damage;
+
+    private Rigidbody rb;
+
+    public virtual void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = velocity;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Enemy")
+        {
+            if (other.tag == "Player")
+            {
+                Health h = other.GetComponent<Health>();
+                h.ApplyDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
