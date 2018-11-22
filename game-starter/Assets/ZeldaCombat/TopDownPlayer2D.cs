70fc93b9e2fe66af017057da487c0d165a92303e
Anton Pantev <antonpantev123@gmail.com>
2018-11-21 19:00:33 -0600

    Working on top down combat

---
+++ game-starter/Assets/ZeldaCombat/TopDownPlayer2D.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace GameStarter
+{
+    public class TopDownPlayer2D : MonoBehaviour
+    {
+        public float accel = 50f;
+        public float stoppingDrag = 25f;
+        public float maxSpeed = 6f;
+
+        public float knockbackAccel = 600f;
+
+        Rigidbody2D rb;
+        float x;
+        float y;
+        bool knockback;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            x = Input.GetAxisRaw("Horizontal");
+            y = Input.GetAxisRaw("Vertical");
+
+            knockback |= Input.GetButtonDown("Jump");
+        }
+
+        void FixedUpdate()
+        {
+            if (Utils.IsZero(x) && Utils.IsZero(y))
+            {
+                rb.drag = stoppingDrag;
+            }
+            else
+            {
+                rb.drag = 0f;
+                Vector2 force = new Vector2(x, y);
+                force = force.normalized * accel;
+                rb.AddForce(force);
+            }
+
+            if(knockback) {
+                rb.AddForce(Random.insideUnitCircle.normalized * knockbackAccel);
+                knockback = false;
+            }
+
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+            }
+        }
+    }
+}
---
+++ game-starter/Assets/ZeldaCombat/notes.md
@@ -0,0 +1,3 @@
+Using simple impulses for knockback works but is imperfect. 
+* The impulse needs to be a much higher number than the sustained force for movement.
+* The impulse moves the character more when player is moving then when the player is stationary (since the player turns off drag during movement)
